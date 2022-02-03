0dcfc14d9f127ff4d9294f6426bb74f1ec1bc6a4
cameron <cameron.studdstreet@gmail.com>
2022-02-03 07:58:04 +0000

    0.6.4

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
-//! A library to ease creation of so-called "microtypes".
+#![warn(clippy::all)]
+#![warn(rustdoc::all)]
+
+//! A library to generate "microtypes" (A.K.A. "newtypes").
 //!
-//! A microtype is a thin wrapper around a more primitive type (e.g. `String`), that can help to provide
-//! safety against logic bugs at compile-time.
+//! A microtype is a thin wrapper around a more primitive type (e.g. `String`), that helps prevent 
+//! logic and security bugs at compile time.
 //!
-//! For example, consider the following code from an imaginary e-commerce app:
+//! For example, consider the following code from an imaginary e-commerce web backend:
 //! ```
 //! # #[macro_use]
 //! # extern crate microtype;
@@ -23,19 +26,21 @@
 //!
 //! This code compiles, but has a bug: the `order_id` and `user_id` are used in the wrong order.
 //! This example is fairly trivial and easy to spot, but the larger a project gets, the harder it
-//! becomes to spot these issues. Not only that, when refactoring, wouldn't it be nice if the
-//! compiler could highlight all the places you needed to change for you?
+//! becomes to spot these issues. This becomes especially troublesome if you want to refactor. For
+//! example, if you wanted to swap the order of the arguments, you'd have to make sure you visited
+//! all the calls to this function and swapped the arguments manually. What if we could make the
+//! compiler do this?
 //!
-//! For this, we use microtypes. Microtypes are thin wrappers around other types, and tell the
-//! compiler about different uses for the same underlying data. For example, if we rewrite our
-//! earlier code to use microtypes, it might look like this:
+//! Microtypes solve this problem. They wrap some inner type, and allow the compiler to distinguish
+//! between different uses of the same underlying type. For example, we could rewrite the earlier
+//! example as:
 //!
 //! ```compile_fail
 //! # #[macro_use]
 //! # extern crate microtype;
 //! # fn retrieve_user_id() -> String { "".into() }
 //! # fn retrieve_order_id() -> String { "".into() }
-//! // microtype wrappers around String
+//! // Generate wrappers around String called UserId and OrderId
 //! microtype! {
 //!     String {
 //!         UserId,
@@ -58,18 +63,34 @@
 //!
 //! ## Basic usage
 //!
-//! To create a microtype, use the `microtype` macro. Microtypes also implement the `Microtype`
-//! trait which defines some common behaviours:
+//! To declare microtypes, use the ['microtype::microtype'] macro:
 //! ```
 //! # #[macro_use]
 //! # extern crate microtype;
 //! # use microtype::Microtype;
 //! microtype! {
+//!     // these attributes apply to all microtypes defined in this block
 //!     #[derive(Debug, Clone)]
 //!     String {
 //!         #[derive(PartialEq)]
 //!         UserId,    // implements Debug, Clone and PartialEq
+//!
 //!         Username,  // only implements Debug and Clone
+//!     }
+//!
+//!     // multiple inner types can be used in a single macro
+//!     i64 {
+//!         Timestamp
+//!     }
+//!
+//!     // use `secret` to mark a type as secret
+//!     secret String {
+//!         Password
+//!     }
+//!
+//!     // use `out secret` to make a secret type implement serde::Serialize
+//!     out secret String {
+//!         SessionToken
 //!     }
 //! }
 //!
@@ -81,7 +102,7 @@
 //!     // sometimes you need to explicitly change the type of a value:
 //!     let user_id: UserId = username.transmute();
 //!
-//!     // microtypes also implement Deref 
+//!     // microtypes also implement Deref
 //!     let length = user_id.len();
 //!     assert_eq!(length, 2);
 //! }
@@ -114,8 +135,8 @@
 //!     assert_eq!(password.expose_secret(), "secret password");
 //! }
 //! ```
-//! Secret microtypes don't implement [`microtype::Microtype`], instead they implement
-//! [`microtype::SecretMicrotype`], which has a much more restrictive API:
+//! Secret microtypes don't implement [`Microtype`], instead they implement
+//! [`SecretMicrotype`], which has a much more restrictive API:
 //!  - Mutable and owned access to the inner data is not possible, it is only possible to get a
 //!  shared reference to the inner data via [`secrecy::ExposeSecret::expose_secret`].
 //!  - They `#[derive(Debug, Clone)]` (and optionally `Serialize` and `Deserialize`) but do not support adding extra derive macros.
@@ -129,9 +150,9 @@
 //!  ensures sensitive data exists in memory only for as long as is needed. (Caveat: not all types
 //!  have perfect zeroization implementations, notably `Vec`, etc. will not be able to zeroize
 //!  previous allocations)
-//!  - when using `serde`, secret microtypes do not implement [`serde::Serialize`]. For example, in
+//!  - when using `serde`, secret microtypes do not implement `Serialize`. For example, in
 //!  a web server, using a secret microtype for `Password`s would prevent a password from being
-//!  sent in a web response, at compile time.
+//!  sent in a web response, checked at compile time.
 //!
 //! ## Serializable Secrets
 //!
@@ -139,7 +160,7 @@
 //! reasonably be considered "sensitive", but you are likely going to want to serialize them at
 //! some point.
 //!
-//! For this purpose, there is an escape hatch. By declaring an `out secret`, a [`serde::Serialize`] 
+//! For this purpose, there is an escape hatch. By declaring a microtype as an `out secret`, a `Serialize`
 //! implementation will be generated for your microtype:
 //!
 //! ```
@@ -156,12 +177,14 @@
 //!     token: SessionToken
 //! }
 //! ```
+//!
+//! ## Feature Flags
+//! - `serde_impls` - automatically generate `serde` implementations for secret microtypes
+//! - `test_impls` - generate `PartialEq` implementations in tests
 
-
-
+pub use microtype_core::secrecy;
+pub use microtype_core::{Microtype, SecretMicrotype};
 pub use microtype_macro::microtype;
-pub use microtype_core::*;
-pub use secrecy;
 
 #[cfg(test)]
 #[test]
